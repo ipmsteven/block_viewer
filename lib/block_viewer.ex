7b6b876e9090874b914604d74ea82e2627ed45f6
yunlei <yunlei.liu@appfolio.com>
2018-05-16 23:46:35 -0700

    use `with` for better pipeline error handling

--- lib/block_viewer.ex
+++ lib/block_viewer.ex
@@ -1,51 +1,43 @@
 defmodule BlockViewer do
-
   alias BlockViewer.Model.Block
   alias BlockViewer.Parser.BlockParser
+  alias BlockViewer.CLI.ArgsParser
+  alias BlockViewer.Requester.BlockHttpRequester
   alias BlockViewer.Formatter.BlockFormatter
 
-  @moduledoc """
-  Documentation for BlockViewer.
-  """
-  def main([]) do
-    IO.puts("Please provide a hash you want query")
-  end
-
   def main(args) do
-    args
-    |> List.first
-    |> get_block_info
-    |> pick_top_five_txns
-    |> format_block
-    |> IO.puts
+    with {:ok, data} <- parse_args(args),
+	 {:ok, data} <- get_raw_block_info(data),
+	 {:ok, data} <- parse_raw_block_info(data),
+	 {:ok, data} <- pick_top_five_txns(data),
+	 {:ok, data} <- format_block(data)
+    do
+      IO.puts data
+    else
+      {:error, error} -> IO.puts "#{error}"
+    end
   end
 
-  defp get_block_info(hash) do
-    url_for(hash)
-    |> HTTPoison.get
-    |> parse_response
+  defp parse_args(args) do
+    args |> ArgsParser.parse
+  end
+
+  defp get_raw_block_info(hash) do
+    url = "https://webbtc.com/block/#{hash}.bin"
+    url |> BlockHttpRequester.fetch
+  end
+
+  defp parse_raw_block_info(block) do
+    block |> BlockParser.parse
   end
 
   defp pick_top_five_txns(block) do
     %Block{txns: txns} = block
-
-    %{block | txns: Enum.take(txns, 5)}
+    {:ok, %{block | txns: Enum.take(txns, 5)}}
   end
 
   defp format_block(block) do
-    BlockFormatter.format(block)
-  end
-
-  defp url_for(hash) do
-    "https://webbtc.com/block/#{hash}.bin"
-  end
-
-  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
-    body |> BlockParser.parse
-  end
-
-  defp parse_response(_) do
-    :error
+    {:ok, BlockFormatter.format(block)}
   end
 end
 
